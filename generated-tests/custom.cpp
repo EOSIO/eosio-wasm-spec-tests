82f9ed371f43cd1899e38e1009f08dfac4937555
Jeffrey Smith II <jeffreyssmith2nd@gmail.com>
2019-08-30 14:41:48 -0400

    Add generated tests

---
+++ generated-tests/custom.cpp
@@ -0,0 +1,65 @@
+#include <wasm_spec_tests.hpp>
+
+const string wasm_str_custom_0 = base_dir + "/eosio-wasm-spec-tests/generated-tests/wasms/custom.0.wasm";
+std::vector<uint8_t> wasm_custom_0= read_wasm(wasm_str_custom_0.c_str());
+
+BOOST_DATA_TEST_CASE(custom_0_module, boost::unit_test::data::xrange(0,1), index) { try {
+   TESTER tester;
+   tester.produce_block();
+   tester.create_account( N(wasmtest) );
+   tester.produce_block();
+   tester.set_code(N(wasmtest), wasm_custom_0);
+   tester.produce_block();
+
+   action test;
+   test.account = N(wasmtest);
+   test.name = account_name((uint64_t)index);
+   test.authorization = {{N(wasmtest), config::active_name}};
+
+   push_action(tester, std::move(test), N(wasmtest).to_uint64_t());
+   tester.produce_block();
+   BOOST_REQUIRE_EQUAL( tester.validate(), true );
+} FC_LOG_AND_RETHROW() }
+
+const string wasm_str_custom_1 = base_dir + "/eosio-wasm-spec-tests/generated-tests/wasms/custom.1.wasm";
+std::vector<uint8_t> wasm_custom_1= read_wasm(wasm_str_custom_1.c_str());
+
+BOOST_DATA_TEST_CASE(custom_1_module, boost::unit_test::data::xrange(0,1), index) { try {
+   TESTER tester;
+   tester.produce_block();
+   tester.create_account( N(wasmtest) );
+   tester.produce_block();
+   tester.set_code(N(wasmtest), wasm_custom_1);
+   tester.produce_block();
+
+   action test;
+   test.account = N(wasmtest);
+   test.name = account_name((uint64_t)index);
+   test.authorization = {{N(wasmtest), config::active_name}};
+
+   push_action(tester, std::move(test), N(wasmtest).to_uint64_t());
+   tester.produce_block();
+   BOOST_REQUIRE_EQUAL( tester.validate(), true );
+} FC_LOG_AND_RETHROW() }
+
+const string wasm_str_custom_2 = base_dir + "/eosio-wasm-spec-tests/generated-tests/wasms/custom.2.wasm";
+std::vector<uint8_t> wasm_custom_2= read_wasm(wasm_str_custom_2.c_str());
+
+BOOST_DATA_TEST_CASE(custom_2_module, boost::unit_test::data::xrange(0,1), index) { try {
+   TESTER tester;
+   tester.produce_block();
+   tester.create_account( N(wasmtest) );
+   tester.produce_block();
+   tester.set_code(N(wasmtest), wasm_custom_2);
+   tester.produce_block();
+
+   action test;
+   test.account = N(wasmtest);
+   test.name = account_name((uint64_t)index);
+   test.authorization = {{N(wasmtest), config::active_name}};
+
+   push_action(tester, std::move(test), N(wasmtest).to_uint64_t());
+   tester.produce_block();
+   BOOST_REQUIRE_EQUAL( tester.validate(), true );
+} FC_LOG_AND_RETHROW() }
+
